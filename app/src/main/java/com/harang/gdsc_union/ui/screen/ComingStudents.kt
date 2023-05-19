b7c6db951091587952d555b5e15c8e76cac58cfa
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-05-20 05:02:45 +0900

    Update

--- app/src/main/java/com/harang/gdsc_union/ui/screen/ComingStudents.kt
+++ app/src/main/java/com/harang/gdsc_union/ui/screen/ComingStudents.kt
@@ -1,20 +1,79 @@
 package com.harang.gdsc_union.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun ComingStudents() {
-    Text(
+    Column(
         modifier = Modifier
-            .padding(20.dp),
-        text = "찾아오는 제자들",
-        fontSize = 30.sp,
-        fontWeight = FontWeight.Bold
-    )
+            .fillMaxSize()
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(20.dp),
+            text = "찾아오는 제자들",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(1),
+            contentPadding = PaddingValues(20.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            items(8) {it ->
+                Items(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun Items(
+    idx: Int
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth()
+                .background(
+                    color = Color(0xffbbffbb),
+                    shape = RoundedCornerShape(8.dp)
+                ),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(start = 20.dp),
+                text = "학생${idx}",
+                fontSize = 20.sp
+            )
+        }
+    }
 }
